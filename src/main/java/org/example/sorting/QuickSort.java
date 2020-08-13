773f91216aba475c21293f5907ed09a58a311589
Daniel In Dallas <danielsmart.an@gmail.com>
2020-08-13 01:56:58 -0500

    QuickSort

---
+++ src/main/java/org/example/sorting/QuickSort.java
@@ -0,0 +1,72 @@
+package org.example.sorting;
+
+// Code from geeksforgeeks website
+
+/*
+We start from the leftmost element and keep track of index of smaller (or equal to) elements as i.
+While traversing, if we find a smaller element, we swap current element with arr[i].
+Otherwise we ignore current element.
+*/
+
+public class QuickSort {
+    /*  This function takes the last element as pivot,
+        places the pivot element at its correct position
+        in sorted array, and places all smaller(than pivot)
+        to left of pivot and greater elements to right of
+        pivot */
+    int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // swap arr[i] and arr[j]
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    /*
+        The main function that implements QuickSort()
+        arr[] --> Array to be sorted,
+        low --> Starting index,
+        high --> Ending index
+     */
+    void sort(int[] arr, int low, int high) {
+        if (low < high) {
+            /*
+                pi is partitioning index,
+                arr[pi] is at the right place
+             */
+            int pi = partition(arr, low, high);
+            // Recursively sort elements before partition and after partition
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
+    }
+
+    static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value);
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {10,7,8,9,1,5};
+
+        QuickSort quickSort = new QuickSort();
+        quickSort.sort(arr, 0, arr.length - 1);
+
+        System.out.println("sorted array: ");
+        printArray(arr);
+    }
+}
