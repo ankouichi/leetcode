6ed3d667f8503d41a51777e5e51997bf61bb8d2c
Daniel In Dallas <danielsmart.an@gmail.com>
2020-08-14 02:09:14 -0500

    Update MergeSort.java

--- src/main/java/org/example/sorting/MergeSort.java
+++ src/main/java/org/example/sorting/MergeSort.java
@@ -1,5 +1,7 @@
 package org.example.sorting;
 
+// Code from geeksforgeeks website
+
 /*
 Merge Sort is a Divide and Conquer algorithm.
 It divides input array in two halves, calls itself for the two halves
